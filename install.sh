030640f699dd7d95afee49564464a3d051cec501
Dr. Drang <drdrang@gmail.com>
2010-03-15 15:38:34 -0500

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp Planner\ print.scpt $HOME/Library/Application\ Support/TaskPaper/Scripts/
+mkdir $HOME/bin
+cp html2ps $HOME/bin/
+cp tp2html $HOME/bin/
+cp html2planner $HOME/bin/
+mkdir $HOME/.html2ps
+cp html2ps-planner $HOME/.html2ps/
