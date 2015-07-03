feb8846f6684835e5696456f25a258ce0d920bae
Dr. Drang <drdrang@gmail.com>
2015-07-02 20:34:36 -0500

    Installer script now creates directories for config and AppleScript files.

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 bin=$HOME/Dropbox/bin/
+mkdir -p $HOME/Library/Scripts/Applications/TaskPaper
 cp Planner\ print.scpt $HOME/Library/Scripts/Applications/TaskPaper/
 cp html2ps $bin
 cp tp2html $bin
 cp html2planner $bin
+mkdir -p $HOME/.html2ps
 cp html2psrc-planner $HOME/.html2ps/
